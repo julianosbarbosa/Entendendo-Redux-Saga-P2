550ec20feeb9647d820eb7cbb6c5daa13c01fc47
Juliano <julianotrabalho2016@gmail.com>
2019-11-12 15:59:30 -0300

    UPDATE - interface of app

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
 // import { Container } from './styles';
 
-export default function Main() {
-  return <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: ""
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuario/repositorio"
+            value={this.state.repositoryInput}
+            onchange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong> (A declarative, efficient, and
+              flexible JavaScript library for building user interfaces)
+            </p>
+            <a href="https://github.com/facebook/react">Acessar</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
 }
